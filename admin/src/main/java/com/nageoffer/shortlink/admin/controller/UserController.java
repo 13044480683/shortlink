f8dd20f7bb6938ce77053541e5584f373b345a91
wang <1776815330@qq.com>
2025-09-14 17:50:56 +0800

    Mybatis++语法逻辑要学习 学习框架架构 学习POM配置 用户模块的定义 Springboot基本的注解

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.nageoffer.shortlink.admin.controller;
+
+import com.nageoffer.shortlink.admin.common.convention.result.Result;
+import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
+import com.nageoffer.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/api/shortlink/v1/user/{username}")
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username){
+        return new Result<UserRespDTO>().setCode("0").setData(userService.getUserByUsername(username));
+    }
+}
---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/UserDO.java
@@ -0,0 +1,66 @@
+package com.nageoffer.shortlink.admin.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @description t_user
+ * @author wjz
+ * @date 2025-09-14
+ */
+@TableName("t_user")
+@Data
+public class UserDO implements Serializable {
+    /**
+     * ID 雪花算法
+     */
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+
+    /**
+     * 注销时间戳
+     */
+    private Long deleteTime;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+
+    /**
+     * 删除标识 0：未删除 1：已删除
+     */
+    private Integer delFlag;
+}
