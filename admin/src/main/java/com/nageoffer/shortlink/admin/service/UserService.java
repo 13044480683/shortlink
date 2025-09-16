03e1c4cffdf06ae25d83339e935db56bc0af9680
wang <1776815330@qq.com>
2025-09-16 13:17:14 +0800

    注册 布隆 防止恶意攻击的Service层

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
@@ -2,8 +2,11 @@ package com.nageoffer.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.UserDO;
+import com.nageoffer.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 
 public interface UserService extends IService<UserDO> {
     UserRespDTO getUserByUsername(String username);
+    Boolean getHaveUsername(String name);
+    void registerUser(UserRegisterReqDTO requestparams);
 }
--- admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/UserServiceImpl.java
@@ -1,19 +1,31 @@
 package com.nageoffer.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.nageoffer.shortlink.admin.common.constant.RedisConstants;
+import com.nageoffer.shortlink.admin.common.convention.errorcode.BaseErrorCode;
+import com.nageoffer.shortlink.admin.common.convention.exception.ClientException;
+import com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.nageoffer.shortlink.admin.dao.entity.UserDO;
 import com.nageoffer.shortlink.admin.dao.mapper.UserMapper;
+import com.nageoffer.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
 
-
+    private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    private final RedissonClient redissonClient;
     @Override
     public UserRespDTO getUserByUsername(String username) {
         LambdaQueryWrapper<UserDO> queryWrapper=Wrappers.lambdaQuery(UserDO.class)
@@ -23,4 +35,33 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         BeanUtils.copyProperties(userDO,result);
         return result;
     }
+
+    @Override
+    public Boolean getHaveUsername(String username) {
+        return userRegisterCachePenetrationBloomFilter.contains(username);
+    }
+
+    @Override
+    public void registerUser(UserRegisterReqDTO requestparams) {
+        //先检测用户名是否存在
+        if (getHaveUsername(requestparams.getUsername())){
+            throw new ClientException(BaseErrorCode.USER_NAME_EXIST_ERROR);
+        }else {
+            RLock lock=redissonClient.getLock(RedisConstants.LOCK_USER_REGISTER_KEY+requestparams.getUsername());
+            try {
+                if (lock.tryLock()){
+                    int result=baseMapper.insert(BeanUtil.toBean(requestparams,UserDO.class));
+                    if (result<1){
+                        throw new ClientException(UserErrorCodeEnum.USER_INSERT_ERROR);
+                    }
+                    userRegisterCachePenetrationBloomFilter.add(requestparams.getUsername());
+                }
+                throw new ClientException(UserErrorCodeEnum.USER_EXIST);
+            }finally {
+                lock.unlock();
+            }
+
+        }
+    }
+
 }
