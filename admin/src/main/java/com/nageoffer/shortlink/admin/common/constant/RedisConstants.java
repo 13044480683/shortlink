fed59dbcbab4a70e9a792e7743c384e69c9eb3b7
wang <1776815330@qq.com>
2025-09-16 13:16:21 +0800

    定义恶意注册锁的KEY

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisConstants.java
@@ -0,0 +1,8 @@
+package com.nageoffer.shortlink.admin.common.constant;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisConstants {
+    public static final String LOCK_USER_REGISTER_KEY="shortlink_lock_user_register";
+}
