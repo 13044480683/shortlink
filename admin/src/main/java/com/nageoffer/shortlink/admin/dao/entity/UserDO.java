f9aa91576354927a6a4417489679f7ad0d501c17
wang <1776815330@qq.com>
2025-09-16 13:15:23 +0800

    是否有用户名存在（布隆过滤器 用户注册（检测是否存在 防止恶意攻击

--- admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/UserDO.java
@@ -1,8 +1,9 @@
 package com.nageoffer.shortlink.admin.dao.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-import lombok.NonNull;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -18,7 +19,6 @@ public class UserDO implements Serializable {
     /**
      * ID 雪花算法
      */
-    @NonNull
     private Long id;
 
     /**
@@ -54,15 +54,18 @@ public class UserDO implements Serializable {
     /**
      * 创建时间
      */
+    @TableField(fill = FieldFill.INSERT)
     private Date createTime;
 
     /**
      * 修改时间
      */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
 
     /**
      * 删除标识 0：未删除 1：已删除
      */
+    @TableField(fill = FieldFill.INSERT)
     private Integer delFlag;
 }
