fcdec1f2808dce4b0544ef5892cc4b7bcb066770
wang <1776815330@qq.com>
2025-09-16 13:13:58 +0800

    无脱敏感的返回方式

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/UserActualRespDTO.java
@@ -0,0 +1,57 @@
+package com.nageoffer.shortlink.admin.dto.resp;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 用户返回参数相应
+ */
+@Data
+public class UserActualRespDTO {
+    /**
+     * ID 雪花算法
+     */
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+
+    /**
+     * 注销时间戳
+     */
+    private Long deleteTime;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+
+    /**
+     * 删除标识 0：未删除 1：已删除
+     */
+    private Integer delFlag;
+
+}
