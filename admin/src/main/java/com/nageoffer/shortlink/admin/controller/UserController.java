8e8eb753a526b807971807f81fe8544ffc451e75
wang <1776815330@qq.com>
2025-09-16 13:14:50 +0800

    是否有用户名存在（布隆过滤器 用户注册（检测是否存在 防止恶意攻击

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.nageoffer.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.nageoffer.shortlink.admin.common.convention.result.Result;
 import com.nageoffer.shortlink.admin.common.convention.result.Results;
+import com.nageoffer.shortlink.admin.dto.req.UserRegisterReqDTO;
+import com.nageoffer.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +18,18 @@ public class UserController {
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username){
         return Results.success(userService.getUserByUsername(username));
+    }
+    @GetMapping("/api/shortlink/v1/actualuser/{username}")
+    public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username){
+        return Results.success(BeanUtil.toBean(userService.getUserByUsername(username),UserActualRespDTO.class));
+    }
+    @GetMapping("/api/shortlink/v1/actualuser/has-name")
+    public Result<Boolean> getHaveUsername(@RequestParam String username){
+        return Results.success(!userService.getHaveUsername(username));
+    }
+    @PostMapping("/api/shortlink/v1/user/register")
+    public Result<Void> registerUser(@RequestBody UserRegisterReqDTO requestParams){
+        userService.registerUser(requestParams);
+        return Results.success();
     }
 }
